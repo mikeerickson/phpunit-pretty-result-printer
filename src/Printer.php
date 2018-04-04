4e53c12ed951fc6e22f7d525fbaae8bb3196bbcb
Mike Erickson <codedungeon@gmail.com>
2018-04-04 13:41:31 -0700

    Apply fixes from StyleCI (#61)

--- src/Printer.php
+++ src/Printer.php
@@ -134,7 +134,7 @@ class Printer extends _ResultPrinter
         $this->configuration = new Config($this->configFileName);
 
         $this->maxNumberOfColumns = $this->getWidth();
-        $this->maxClassNameLength = min((int)($this->maxNumberOfColumns / 2), $this->maxClassNameLength);
+        $this->maxClassNameLength = min((int) ($this->maxNumberOfColumns / 2), $this->maxClassNameLength);
 
         // setup module options
         $this->printerOptions = $this->configuration->all();
@@ -354,7 +354,7 @@ class Printer extends _ResultPrinter
 
         // 'stty size' output example: 36 120
         if (\count($out) > 0) {
-            $width = (int)explode(' ', array_pop($out))[1];
+            $width = (int) explode(' ', array_pop($out))[1];
         }
 
         // handle CircleCI case (probably the same with TravisCI as well)
